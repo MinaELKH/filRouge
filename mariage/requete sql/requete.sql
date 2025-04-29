b19dc8f237ac2a192409b127591502efae8da04d
MinaELKH <mina.elkhotfi@gmail.com>
2025-04-29 10:05:15 +0100

    réalisation des requetes de statistque pour le prestataire

--- mariage/requete sql/requete.sql
+++ mariage/requete sql/requete.sql
@@ -35,3 +35,39 @@ FROM services s
          JOIN categories c ON s.category_id = c.id
          JOIN villes v ON s.ville_id = v.id
 WHERE s.id = 20;
+
+
+//nombre de service pour un prestataire partie statistique
+
+SELECT c.name as categorie ,   count(*) as nbService
+FROM services s
+         JOIN users u ON s.user_id = u.id
+         JOIN categories c ON s.category_id = c.id
+
+WHERE u.id = 3
+group by categorie
+
+
+// nombre de reservation selon statut pour un prestataire statistque
+
+SELECT
+    r.status as status ,
+    count(*) as nbReservation
+from reservations r
+         join services s on r.service_id = s.id
+         join users prestataire on prestataire.id = s.user_id
+where prestataire.id = 3
+group by  r.status
+
+
+//nombre de devis selon statut accpeted (confirmer par le client) pour un prestataire
+
+SELECT
+    count(*) as nbDevis ,
+    d.status as status
+from devis d
+         join reservations r on r.id = d.reservation_id
+         join services s on r.service_id = s.id
+         join users prestataire on prestataire.id = s.user_id
+where prestataire.id = 3
+group by  d.status
